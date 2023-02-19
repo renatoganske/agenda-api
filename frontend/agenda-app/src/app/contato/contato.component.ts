8463c1cd2309da13923770a21a6f24cbd940f85e
Renato Ganske Junior <renatoganske.jr@gmail.com>
2023-02-19 10:59:51 -0300

    feat: implementação de modal para exibição de detalhes do contato

--- frontend/agenda-app/src/app/contato/contato.component.ts
+++ frontend/agenda-app/src/app/contato/contato.component.ts
@@ -3,6 +3,9 @@ import { ContatoService } from '../contato.service';
 import { Contato } from './contato';
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { MatDialog } from '@angular/material/dialog'
+
+import { ContatoDetalheComponent } from '../contato-detalhe/contato-detalhe.component'
 
 @Component({
   selector: 'app-contato',
@@ -13,11 +16,12 @@ export class ContatoComponent implements OnInit {
 
   formulario: FormGroup;
   contatos: Contato[] = [];
-  colunas = [ 'foto', 'id', 'nome', 'email', 'favorito']
+  colunas = ['foto', 'id', 'nome', 'email', 'favorito']
 
   constructor(
     private service: ContatoService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
@@ -46,13 +50,13 @@ export class ContatoComponent implements OnInit {
 
   uploadFoto(event, contato) {
     const files = event.target.files;
-    if(files){
+    if (files) {
       const foto = files[0];
       const formData: FormData = new FormData();
       formData.append("foto", foto);
       this.service
-          .upload(contato, formData)
-          .subscribe(response => this.listarContatos());
+        .upload(contato, formData)
+        .subscribe(response => this.listarContatos());
     }
 
   }
@@ -63,6 +67,14 @@ export class ContatoComponent implements OnInit {
     this.service.save(contato).subscribe(resposta => {
       let lista: Contato[] = [... this.contatos, resposta]
       this.contatos = lista;
+    })
+  }
+
+  visualizarContato(contato: Contato){
+    this.dialog.open(ContatoDetalheComponent, {
+      width: '400px',
+      height:'450px',
+      data: contato
     })
   }
 
