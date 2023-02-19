80302216f8f7f421a4cd5d8514c1c4ba1d1b1f42
Renato Ganske Junior <renatoganske.jr@gmail.com>
2023-02-19 00:16:37 -0300

    feat: adicionando novo contato na lista

--- frontend/agenda-app/src/app/contato/contato.component.ts
+++ frontend/agenda-app/src/app/contato/contato.component.ts
@@ -48,8 +48,8 @@ export class ContatoComponent implements OnInit {
     const formValues = this.formulario.value;
     const contato: Contato = new Contato(formValues.nome, formValues.email);
     this.service.save(contato).subscribe(resposta => {
-      this.contatos.push(resposta);
-      console.log(this.contatos)
+      let lista: Contato [] = [ ... this.contatos, resposta ]
+      this.contatos = lista;
     })
   }
 
